4f0ed580bcf170ebeb93feb3efe5b640925b8ff4
Abdulquadri Adedayo Ismail <ismaildeve070@gmail.com>
2023-02-22 09:27:21 +0100

    test:add listing and listing details

--- __e2e__/App.spec.js
+++ __e2e__/App.spec.js
@@ -9,4 +9,18 @@ describe("Main", () => {
   it("should render listing screen", async () => {
     await expect(element(by.id("listings"))).toBeVisible()
   })
+
+  it("should render add listing screen", async () => {
+    await element(by.id("add-icon")).tap()
+    await expect(element(by.id("step"))).toBeVisible()
+    await expect(element(by.id("listing-name"))).toBeVisible()
+    await element(by.id("close")).tap()
+  })
+
+  it("should render listing details screen", async () => {
+    await element(by.id("listing-card")).atIndex(0).tap()
+    await element(by.id('swipe')).atIndex(0).swipe('left');
+    await element(by.id('swipe')).atIndex(1).swipe('left');
+    await expect(element(by.id("listing-title"))).toBeVisible()
+  })
 })
